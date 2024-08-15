b3d9b71a71944c1bd1ea4768202b0e9873960f46
bilalparray <parraybilal34@gmail.com>
2024-08-15 12:35:01 +0530

    signuprequest validation

--- src/app/main/website/internal/footer/footer.component.ts
+++ src/app/main/website/internal/footer/footer.component.ts
@@ -1,45 +1,58 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   signUpRequestForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private ngxService: NgxUiLoaderService
+  ) {}
   ngOnInit() {
     this.signUpRequestForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
     });
   }
   requestSignUp() {
-   
     if (this.signUpRequestForm.invalid) {
-      Swal.fire({
-        title: 'Error',
-        text: "Enter Valid Email Id.!",
-        icon: "warning"
-      });
+      this.signUpRequestForm.markAllAsTouched();
       return;
+    } else {
+      this.ngxService.startLoader('loader-02');
+      emailjs
+        .send(
+          'service_ei8blie',
+          'template_744gfz9',
+          this.signUpRequestForm.value,
+          'bkcRq_ireBK7U1gGY'
+        )
+        .then(
+          (result: EmailJSResponseStatus) => {
+            this.ngxService.stopLoader('loader-02');
+            Swal.fire({
+              title: result.text,
+              text: 'Your Sign-Up request  has been sent successfully! We will Contact You Soon',
+              icon: 'success',
+            });
+            this.signUpRequestForm.reset();
+          },
+          (error) => {
+            this.ngxService.stopLoader('loader-02');
+            Swal.fire({
+              title: error.text,
+              text: 'There was an error sending your Request.!',
+              icon: 'warning',
+            });
+          }
+        );
     }
-    emailjs.send('service_ei8blie', 'template_744gfz9', this.signUpRequestForm.value, 'bkcRq_ireBK7U1gGY')
-      .then((result: EmailJSResponseStatus) => {
-        Swal.fire({
-          title: result.text,
-          text: "Your Sign-Up request  has been sent successfully! We will Contact You Soon",
-          icon: "success"
-        });
-      }, (error) => {
-        Swal.fire({
-          title: error.text,
-          text: "There was an error sending your Request.!",
-          icon: "warning"
-        });
-      });
   }
 }
